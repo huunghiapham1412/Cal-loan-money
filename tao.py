61846b4d28d29cb478f65e24528e2fab87a7eede
Huu Nghia Pham <huunghiapham1412@gmail.com>
2024-06-13 13:36:05 -0400

    .

--- tao.py
+++ tao.py
@@ -1,8 +1,5 @@
 import pandas as pd
 
-# Tạo một DataFrame trống với các cột thích hợp
 columns = ["Husband Name", "Wife Name", "Husband ID", "Wife ID", "Address", "Collateral Value", "Loan Amount", "Loan Duration", "Interest Rate", "Interest Amount", "Total Payment"]
 df = pd.DataFrame(columns=columns)
-
-# Lưu DataFrame này vào tệp Excel mới
 df.to_excel("loan_data.xlsx", index=False, engine='openpyxl')
